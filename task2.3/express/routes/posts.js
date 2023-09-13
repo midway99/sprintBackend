b0ef2c2905ccd5e29d4baab51a19b6059d7fadfa
Nikita Vystoropsky <nikita.vystoropsky@gmail.com>
2023-09-13 17:29:22 +0300

    fix(task2.3):Добавил структуру проекта и сделал crud

--- task2.3/express/routes/posts.js
+++ task2.3/express/routes/posts.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const postsController = require('../controllers/postsController')
+const postsController = require('../controllers/PostController')
 
 router.get('/', postsController.all);
 router.post('/', postsController.store);
 router.get('/:id', postsController.show);
 router.put('/:id', postsController.update);
-router.delete('/:id', postsController.destroy);
+router.delete('/:id', postsController.remove);
 
 module.exports = router;
