3cb047b803750fc089b7c074184288a5462830e6
Nikita Vystoropsky <nikita.vystoropsky@gmail.com>
2023-09-24 23:46:58 +0300

    feat(task3.1):add socket.io

---
+++ task3.1/express/app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const {createServer} = require('http');
+const {Server} = require('socket.io');
+const http = require("http");
+
+var app = express();
+    const httpServer = createServer(app);
+const io = new Server(httpServer,{});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//Подключение к сокету
+io.on('connection',(socket)=>{
+})
+
+setTimeout(() => io.emit('message', 'Hello world!'), 3000)
+
+httpServer.listen(3000);
+
+module.exports = app;
